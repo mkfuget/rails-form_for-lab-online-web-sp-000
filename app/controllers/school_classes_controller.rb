230750cfe8472fd061f679f9fcb4ff2631793d6e
Michael Fuget <michaelfuget@gmail.com>
2021-01-23 02:40:13 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -14,13 +14,13 @@ class SchoolClassesController < ApplicationController
   def create
     @school_class = SchoolClass.new(school_class_params)
     @school_class.save
-    redirect_to student_path(@school_class)
+    redirect_to school_class_path(@school_class)
   end
 
   def update
 		@school_class = SchoolClass.find(params[:id])
 	  @school_class.update(school_class_params)
-	  redirect_to student_path(@school_class)
+	  redirect_to school_class_path(@school_class)
 	end
 
 	def edit
